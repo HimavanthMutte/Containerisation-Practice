2db546444855073f56d5a56b1d84040de6d502e8
HimavanthMutte <Himu.mutte5@gmail.com>
2026-07-03 13:32:21 +0530

    TypeScript , Ts + Cra , Ts + Vite added

---
+++ practice5/index.js
@@ -0,0 +1,11 @@
+import http from 'http';
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World Backend (Practice 5)');
+});
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
+//# sourceMappingURL=index.js.map
